20674eb7767650d9470a2bebd3f7e49b75ff7d8e
gogate <gaurav.gogate@gmail.com>
2017-10-22 11:17:22 +0530

    initial

---
+++ webapp/src/main/java/com/gogate/apps/HelloController.java
@@ -0,0 +1,16 @@
+package com.gogate.apps;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String sayHello(@RequestParam("name") String name, Model model){
+		model.addAttribute("name1", name);
+		return "home";
+	}
+}
